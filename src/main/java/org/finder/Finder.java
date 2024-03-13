37bd05be158b20d4c6c86193d8d5935cc84e26b5
GANZO9055 <kosta.maslihov@gmail.com>
2024-03-14 00:24:21 +0300

    Тестовое задание. Утилита "поиск файлов". [#783]

--- src/main/java/org/finder/Finder.java
+++ src/main/java/org/finder/Finder.java
@@ -3,14 +3,14 @@ package org.finder;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 public class Finder {
-    private static String path;
-    private static List<String> param = List.of("d", "n", "t", "o");
-    private static List<String> valuesFile = List.of("name", "mask", "regex");
 
     private static void checkArgs(String[] args) {
         if (args.length != 4) {
@@ -18,10 +18,43 @@ public class Finder {
         }
     }
 
-    public static void writeFiles(List<Path> files) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+    private static String convertMaskToRegex(String paramOne) {
+        return paramOne.replace("?", "\\S{1}")
+                .replace(".", "\\.")
+                .replace("*", "\\S*");
+    }
+
+    public static Predicate<Path> searchPath(String name, String type) {
+        if (type.equals("name")) {
+            return path -> path.getFileName().toString().equals(name);
+        }
+        if (type.equals("mask")) {
+            return path -> path.getFileName().toString()
+                    .matches(convertMaskToRegex(name));
+        }
+        if (type.equals("regex")) {
+            Pattern pattern = Pattern.compile(name);
+            return path -> pattern.matcher(path.getFileName().toString()).matches();
+        }
+        throw new IllegalArgumentException("Invalid type.");
+    }
+
+    public static List<Path> searchFiles(String directory, Predicate<Path> predicate) {
+        Path path = Paths.get(directory);
+        SearchFiles searchFiles = new SearchFiles(predicate);
+
+        try {
+            Files.walkFileTree(path, searchFiles);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return searchFiles.getPaths();
+    }
+
+    public static void writeFiles(List<Path> files, String writeToFile) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(writeToFile))) {
             for (Path file : files) {
-                writer.write(file.toString());
+                writer.write(file.toString() + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -31,8 +64,15 @@ public class Finder {
     public static void main(String[] args) {
         checkArgs(args);
 
-        List<Path> list = new ArrayList<>();
+        ArgsName argsName = ArgsName.of(args);
+        String directory = argsName.get("d");
+        String name = argsName.get("n");
+        String type = argsName.get("t");
+        String path = argsName.get("o");
 
-        writeFiles(list);
+        Predicate<Path> predicate = searchPath(name, type);
+        List<Path> list = searchFiles(directory, predicate);
+
+        writeFiles(list, path);
     }
 }
